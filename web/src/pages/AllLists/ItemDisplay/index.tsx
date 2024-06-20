0ff23902be4a0b3e655a73ff1aaed18f8e475dd0
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-06-20 13:17:09 +0530

    feat(web): redirect-main-curate-item-to-list

--- web/src/pages/AllLists/ItemDisplay/index.tsx
+++ web/src/pages/AllLists/ItemDisplay/index.tsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import History from "components/HistoryDisplay";
 import { useItemDetailsQuery } from "queries/useItemDetailsQuery";
 import { useRegistryDetailsQuery } from "queries/useRegistryDetailsQuery";
 import ItemInformationCard from "components/InformationCards/ItemInformationCard";
+import { MAIN_CURATE_ADDRESS } from "src/consts";
 
 const ItemDisplay: React.FC = () => {
   const { itemId } = useParams();
   const [, listAddress] = itemId?.split("@");
+  const navigate = useNavigate();
+
   const { data: itemDetails } = useItemDetailsQuery(itemId);
   const { data: registryDetails } = useRegistryDetailsQuery(listAddress);
+
+  // redirect to list page if the item belongs to main curate
+  useEffect(() => {
+    if (listAddress === MAIN_CURATE_ADDRESS && itemDetails?.item?.key0) {
+      navigate(`/lists/${itemDetails.item.key0}-${itemId}/display/1/desc/all`);
+    }
+  }, [itemId, itemDetails]);
 
   return (
     <div>

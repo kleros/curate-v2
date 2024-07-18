e38ac91285cfe89f4a3c463bddaff399eebea332
Shubham Parkhi <shubhamparkhi10@gmail.com>
2024-07-18 06:42:52 +0530

    fix(web): fix overlay and cleanup

--- web/src/layout/Header/index.tsx
+++ web/src/layout/Header/index.tsx
@@ -28,7 +28,7 @@ export const PopupContainer = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  z-index: 1;
+  z-index: 30;
   background-color: ${({ theme }) => theme.blackLowOpacity};
 `;
 

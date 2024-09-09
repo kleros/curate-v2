e3e2e0fc009f55bcaab702f81d4296f719387853
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-09-09 12:39:03 +0530

    fix(web): incorrect-court-site-url

--- web/vite.config.js
+++ web/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       },
     },
   },
-  envPrefix: ["REACT_APP", "ALCHEMY", "WALLETCONNECT_PROJECT_ID"],
+  envPrefix: ["REACT_APP", "ALCHEMY", "WALLETCONNECT_PROJECT_ID", "COURT_SITE"],
   plugins: [
     svgr({
       include: ["**/*.svg", "tsx:**/*.svg"],

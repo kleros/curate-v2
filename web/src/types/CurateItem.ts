03de704a254b915d19de4d26bc5b0bebed4b856a
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-03-10 12:41:07 +0530

    chore: json-docs-for-curate-docs

--- web/src/types/CurateItem.ts
+++ web/src/types/CurateItem.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
 
+/**
+ * @description Schema for Curate Item
+ * @example https://ipfs.io/ipfs/QmPoiGE34wnjzRVcSEK3gH2jL4sszyY1P2tpMeiiWFXysZ/item.json
+ */
 export const curateItemSchema = z.object({
   columns: z.array(
     z.object({
@@ -9,6 +13,7 @@ export const curateItemSchema = z.object({
       type: z.string(),
     })
   ),
+  // for each columns.label, it's value as `columns.label : value`
   values: z.record(z.string(), z.string()),
 });
 

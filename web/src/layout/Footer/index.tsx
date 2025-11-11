b5b02b9afff1399a43154e460252508e117b3252
kyrers <kyrers.eth@gmail.com>
2025-11-11 19:33:15 +0000

    refactor: replace Link components with anchor tags in Footer

--- web/src/layout/Footer/index.tsx
+++ web/src/layout/Footer/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import clsx from "clsx";
-import { Link } from "react-router-dom";
 
 import SecuredByKlerosLogo from "svgs/footer/secured-by-kleros.svg";
 
@@ -9,17 +8,17 @@ import { socialmedia } from "consts/socialmedia";
 import LightButton from "components/LightButton";
 
 const SecuredByKleros: React.FC = () => (
-  <Link to="https://kleros.io" target="_blank" rel="noopener noreferrer">
+  <a href="https://kleros.io" target="_blank" rel="noopener noreferrer">
     <SecuredByKlerosLogo className="min-h-6 transition duration-100 [&_path]:fill-white/75 hover:[&_path]:fill-white" />
-  </Link>
+  </a>
 );
 
 const SocialMedia = () => (
   <div className="flex [&_.button-svg]:mr-0">
     {Object.values(socialmedia).map((site, i) => (
-      <Link key={site.url} to={site.url} target="_blank" rel="noopener noreferrer">
+      <a key={site.url} href={site.url} target="_blank" rel="noopener noreferrer">
         <LightButton Icon={site.icon} text="" />
-      </Link>
+      </a>
     ))}
   </div>
 );

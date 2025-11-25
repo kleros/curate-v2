1a7242a41ef08a6592919379eeb8de78f4167380
kyrers <kyrers.eth@gmail.com>
2025-11-25 18:39:11 +0000

    fix: enhance address validation and display in AddressInput

--- web/src/pages/SubmitItem/ItemField/FieldInput/AddressInput.tsx
+++ web/src/pages/SubmitItem/ItemField/FieldInput/AddressInput.tsx
@@ -7,19 +7,26 @@ import { cn } from "src/utils";
 import { LANDSCAPE_WIDTH_CALC } from "./constants";
 
 const AddressInput: React.FC<IFieldInput> = ({ fieldProp, handleWrite }) => {
+  const [address, setAddress] = useState(fieldProp.value ?? "");
   const [isError, setIsError] = useState(false);
+
   const handleChange = (value: string) => {
-    if (!isAddress(value)) {
+    setAddress(value);
+
+    if (isAddress(value)) {
+      setIsError(false);
+      handleWrite(value);
+    } else {
       setIsError(true);
-      return;
+      handleWrite("");
     }
-    handleWrite(value);
   };
+
   return (
     <TextField
       className={cn("w-[80vw]", LANDSCAPE_WIDTH_CALC)}
       style={{ marginBottom: responsiveSize(68, 40) }}
-      value={fieldProp.value}
+      value={address}
       onChange={handleChange}
       variant={isError ? "error" : "info"}
       message={fieldProp.description}
